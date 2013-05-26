3379b9e4186ff989688a70512b96f5ed107a8624
Jonathan Goldsmith <jhghank@gmail.com>
2013-05-25 22:30:17 -0700

    converted to pythonic style

--- bot.py
+++ bot.py
@@ -38,26 +38,26 @@ pronouns = {"i'm": "you're", "i":"you","me":"you","yours":"mine","you":"I","am":
 random.seed()
 print "I'm psychiatrist bot. I can make you feel better. Tell me how you're feeling!"
 
-while(True):
-    input=raw_input("> ") #get input
-    input=input.lower()
-    input=input.rstrip('.!?')
+while True:
+    input = raw_input("> ") #get input
+    input = input.lower()
+    input = input.rstrip('.!?')
     
     for pattern in respPatterns:
-      wildcards=[]
-      if re.match(pattern[0],input)!=None:
-          wildcards=re.split(pattern[0],input)
-          wildcards[:]=[x for x in wildcards if x!='']
-          rand=random.randint(0,len(pattern[1])-1)
-          responseWords=pattern[1][rand].split(' ')
-          response=""
-          for word in responseWords:
-              if word.startswith('%'):
-                  word=word.lstrip('%')
-                  word=wildcards[int(word)]
-                  word=' '.join(pronouns.get(s,s) for s in word.split())
-              response+=word+' '
-          break
+        wildcards = []
+        if re.match(pattern[0], input):
+            wildcards = re.split(pattern[0], input)
+            wildcards = [x for x in wildcards if x]
+            rand = random.randint(0,len(pattern[1]) - 1)
+            responseWords = pattern[1][rand].split(' ')
+            response = "" 
+            for word in responseWords:
+                if word.startswith('%'):
+                    word = word.lstrip('%')
+                    word = wildcards[int(word)]
+                    word = ' '.join(pronouns.get(s,s) for s in word.split())
+                response += word + ' '
+            break
     
     print response
           
